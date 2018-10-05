a28ab3b33df2535d8b694acc79d74b13a0bdf734
liujn2018 <38339308+liujn2018@users.noreply.github.com>
2018-10-05 14:29:31 +0800

    Update paopu2.py

--- paopu2.py
+++ paopu2.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*- 
 import sys
 import os
+import subprocess
+import re
 
 def __shell__(cmd):
   os.system(cmd)
+  
+def __shell2__(cmd):
+  return subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT).decode('utf-8')
 
 if not os.path.exists('/usr/lib/x86_64-linux-gnu/libnvidia-opencl.so.1'):
     __shell__('apt update > /dev/null')
@@ -15,11 +20,28 @@ if not os.path.exists('/usr/lib/x86_64-linux-gnu/libnvidia-opencl.so.1'):
     __shell__('apt -qq install --no-install-recommends nvidia-opencl-dev > /dev/null')
     __shell__('apt --fix-broken install > /dev/null')
 
+__shell__('mkdir -p run_lz_in_gg/networks')
+__shell__('rm -rf wtlist')
+__shell__('git clone https://github.com/liujn2018/wtlist.git')
 __shell__('pip install gdown >/dev/null')
-#d13c40993740cb77d85c838b82c08cc9c3f0fbc7d8c3761366e5d59e8f371cbd
-__shell__('gdown https://drive.google.com/uc?id=12nkQsoyKJ4OP8p4foJ05jTxXATfGnJ1M')
-#c910dee9c057d144efaafb603ce69d564825ca316dd8129ce0f05c1231d1f9bc
-__shell__('gdown https://drive.google.com/uc?id=1srbuq-CqAGOqU8FxgIfCpr7o1fsSQR5C')
-__shell__('mkdir -p run_lz_in_gg/networks; ls | head -2| xargs -i cp -r {} run_lz_in_gg/networks')
+with open('wtlist/current.txt', 'rt') as wt_in:
+    for line in wt_in.readlines():
+        if line.startswith('#'):
+            continue
+        if len(line)<5:
+            continue
+        dlinfo = __shell2__('gdown --id {0}'.format(line))
+        print(dlinfo)
+        match = re.search(r'To: /content/(.*?)$', dlinfo, re.MULTILINE)
+        if match:
+            #print(match.group(1))
+            filename = match.group(1)
+            if filename.endswith('.gz'):
+                __shell__('gunzip -f {0}'.format(filename))
+                filename = filename[:-3]
+            else:
+                pass
+            __shell__('mv /content/{0} run_lz_in_gg/networks/'.format(filename))
+        
 
-__shell__('cd run_lz_in_gg; ./autogtp | grep minute')
+__shell__('cd run_lz_in_gg; ./autogtp -k sgf')
