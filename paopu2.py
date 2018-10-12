9e50ffe35c339792acdcebc50c8ed53468de1a54
liujn2018 <38339308+liujn2018@users.noreply.github.com>
2018-10-12 10:34:19 +0800

    Update paopu2.py

--- paopu2.py
+++ paopu2.py
@@ -10,15 +10,15 @@ def __shell__(cmd):
 def __shell2__(cmd):
   return subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT).decode('utf-8')
 
-if not os.path.exists('/usr/lib/x86_64-linux-gnu/libnvidia-opencl.so.1'):
-    __shell__('apt update > /dev/null')
-    __shell__('apt install qt5-default qt5-qmake curl > /dev/null')
-    __shell__('apt install libboost-dev libboost-program-options-dev libopenblas-dev opencl-headers ocl-icd-opencl-dev > /dev/null')
-    __shell__('apt -qq install --no-install-recommends nvidia-opencl-icd-384 > /dev/null')
-    __shell__('wget http://launchpadlibrarian.net/352962266/nvidia-opencl-icd-384_384.111-0ubuntu0.17.10.1_amd64.deb > /dev/null')
-    __shell__('apt install -f ./nvidia-opencl-icd-384_384.111-0ubuntu0.17.10.1_amd64.deb > /dev/null')
-    __shell__('apt -qq install --no-install-recommends nvidia-opencl-dev > /dev/null')
-    __shell__('apt --fix-broken install > /dev/null')
+#if not os.path.exists('/usr/lib/x86_64-linux-gnu/libnvidia-opencl.so.1'):
+__shell__('apt update > /dev/null')
+__shell__('apt install qt5-default qt5-qmake curl > /dev/null')
+__shell__('apt install libboost-dev libboost-program-options-dev libopenblas-dev opencl-headers ocl-icd-opencl-dev > /dev/null')
+__shell__('apt -qq install --no-install-recommends nvidia-opencl-icd-384 > /dev/null')
+__shell__('wget http://launchpadlibrarian.net/352962266/nvidia-opencl-icd-384_384.111-0ubuntu0.17.10.1_amd64.deb > /dev/null')
+__shell__('apt install -f ./nvidia-opencl-icd-384_384.111-0ubuntu0.17.10.1_amd64.deb > /dev/null')
+__shell__('apt -qq install --no-install-recommends nvidia-opencl-dev > /dev/null')
+__shell__('apt --fix-broken install > /dev/null')
 
 __shell__('mkdir -p run_lz_in_gg/networks')
 __shell__('rm -rf wtlist')
